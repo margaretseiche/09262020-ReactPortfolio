2d0028885bbbe9589ca4222e8fb36cb0195c6a84
Margaret Seiche <margaretseiche@hotmail.com>
2020-10-06 17:49:10 -0400

    Updates

---
+++ precache-manifest.1182263a4983fb513dcacc380184389a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987b6e676ed14775008d0ced7294c442",
+    "url": "/09292020-reactportfolio/index.html"
+  },
+  {
+    "revision": "f5d4977b4b415e660224",
+    "url": "/09292020-reactportfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "bf25e5df9fda093a7179",
+    "url": "/09292020-reactportfolio/static/css/main.a860df01.chunk.css"
+  },
+  {
+    "revision": "f5d4977b4b415e660224",
+    "url": "/09292020-reactportfolio/static/js/2.bd46e412.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/09292020-reactportfolio/static/js/2.bd46e412.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf25e5df9fda093a7179",
+    "url": "/09292020-reactportfolio/static/js/main.8953ef7e.chunk.js"
+  },
+  {
+    "revision": "02f2da9b83d34a2de976",
+    "url": "/09292020-reactportfolio/static/js/runtime-main.9ba33697.js"
+  }
+]);
